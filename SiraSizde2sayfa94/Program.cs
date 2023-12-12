fdefeb5ae916765c1584a8498179a0ccec3bde36
eren595 <eren595@BPC12>
2023-12-12 11:27:59 +0300

    Proje dosyası ekle.

---
+++ SiraSizde2sayfa94/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Odbc;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiraSizde2sayfa94
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Ev ev = new Ev();
+            Console.WriteLine("oda sayısı:" + ev.oda);
+            Console.WriteLine("meter kare:"+ev.metrekare);
+        }
+    }
+    class Ev
+    {
+        public int oda { get; set; }
+        public int metrekare { get; set; }
+        public Ev() 
+        {
+          oda = 5;
+          metrekare = 8;
+        }
+    }
+}
